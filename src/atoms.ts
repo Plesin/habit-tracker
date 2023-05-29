07344b168f4837adcd2b04e48bbc7392f06e31c5
Radek Pleskac <info@radekpleskac.com>
2023-05-29 21:31:04 +0200

    show state for the past week

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from 'recoil'
 import { subDays } from 'date-fns'
-import { getHabits } from '../api'
+import { getHabits, getCompletedDates } from '../api'
 
 export const habitsState = atom({
   key: 'habits',
@@ -17,4 +17,9 @@ for (let i = 6; i >= 0; i--) {
 export const past7DaysState = atom({
   key: 'past7Days',
   default: past7Days,
+})
+
+export const completedDatesState = atom({
+  key: 'completedDates',
+  default: getCompletedDates(),
 })
